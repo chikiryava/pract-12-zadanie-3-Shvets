8d2f68de33c955cd3f13489cb79e5d1e7dd9625c
chikiryava <98350681+chikiryava@users.noreply.github.com>
2023-02-23 15:59:53 +0500

    Add files via upload

---
+++ Robot game/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Robot_game
+{
+    public partial class Form1 : Form
+    {
+        Robot robot1 = new Robot();
+        Robot robot2 = new Robot();
+        Robot robot3 = new Robot();
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if (robot1.SetHp(textBox1.Text,"1") ==true && robot2.SetHp(textBox2.Text,"2") == true && robot3.SetHp(textBox3.Text,"3") == true)
+            {
+                robot1.SetHp(textBox1.Text, "1");
+                robot2.SetHp(textBox2.Text, "2");
+                robot3.SetHp(textBox3.Text, "3");
+                label4.Text = $"Здоровье после игры = {robot1.NewHp()}";
+                label5.Text = $"Здоровье после игры = {robot2.NewHp()}";
+                label6.Text = $"Здоровье после игры = {robot3.NewHp()}";
+            }
+                
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] myarray = new string[3];
+            if (textBox4.Text == "1")
+            {
+                myarray = robot1.HealOtherRobots(textBox1.Text, textBox2.Text, textBox3.Text, 2);
+                textBox1.Text = myarray[0];
+                textBox2.Text = myarray[1];
+                textBox3.Text = myarray[2];
+                button2.Enabled = false;
+            }
+            else if (textBox4.Text == "2")
+            {
+                myarray = robot1.HealOtherRobots(textBox2.Text, textBox1.Text, textBox3.Text, 2);
+                textBox1.Text = myarray[1];
+                textBox2.Text = myarray[0];
+                textBox3.Text = myarray[2];
+                button2.Enabled = false;
+            }
+            else if (textBox4.Text == "3")
+            {
+                myarray = robot1.HealOtherRobots(textBox3.Text, textBox2.Text, textBox1.Text, 2);
+                textBox1.Text = myarray[2];
+                textBox2.Text = myarray[1];
+                textBox3.Text = myarray[0];
+                button2.Enabled = false;
+            }
+            else
+                MessageBox.Show("Введите номер робота(от 1 до 3)");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string[] myarray = new string[3];
+            if (textBox4.Text == "1")
+            {
+                myarray = robot1.HealOtherRobots(textBox1.Text, textBox2.Text, textBox3.Text, 1);
+                textBox1.Text = myarray[0];
+                textBox2.Text = myarray[1];
+                textBox3.Text = myarray[2];
+                button3.Enabled = false;
+            }
+            else if (textBox4.Text == "2")
+            {
+                myarray = robot1.HealOtherRobots(textBox2.Text, textBox1.Text, textBox3.Text, 1);
+                textBox1.Text = myarray[1];
+                textBox2.Text = myarray[0];
+                textBox3.Text = myarray[2];
+                button3.Enabled = false;
+            }
+            else if (textBox4.Text == "3")
+            {
+                myarray = robot1.HealOtherRobots(textBox3.Text, textBox2.Text, textBox1.Text, 1);
+                textBox1.Text = myarray[2];
+                textBox2.Text = myarray[1];
+                textBox3.Text = myarray[0];
+                button3.Enabled = false;
+            }
+            else
+                MessageBox.Show("Введите номер робота(от 1 до 3)");
+
+        }
+    }
+}
---
+++ Robot game/Robot.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Robot_game
+{
+    internal class Robot
+    {
+        private int hp;
+
+        public bool SetHp(string userhp,string robotnumber)
+        {
+
+            if (!int.TryParse(userhp, out var num))
+            {
+                MessageBox.Show($"Вы ввели не число в графе здоровье у робота номер {robotnumber}");
+                return false;
+            }
+            else
+            {
+                int health = int.Parse(userhp);
+                if (health <= 0)
+                {
+                    MessageBox.Show($"Здоровье не может быть меньше нуля у робота номер {robotnumber}");
+                    return false;
+                }
+                else
+                {
+                    hp = health;
+                    return true;
+
+                }
+
+            }                              
+        }
+
+        public string NewHp()
+        {
+            int newhealth = hp / 2;
+            return newhealth.ToString();
+        }
+        
+        public string[] HealOtherRobots(string donor,string robot,string anotherrobot,int buttonnumber)
+        {
+            string[] myarray = new string[3];
+            if (int.TryParse(donor, out var n) && int.TryParse(anotherrobot, out var r) && int.TryParse(robot, out var c))
+            {
+                double donorhp = double.Parse(donor);
+                double robothp = double.Parse(robot);
+                double anotherrobothp = double.Parse(anotherrobot);
+                if (buttonnumber == 1)
+                {
+                    donor = (donorhp / 2).ToString();
+                    robot = (robothp + 30).ToString();
+                    anotherrobot = (anotherrobothp + 30).ToString();
+                    myarray[0] = donor;
+                    myarray[1] = robot;
+                    myarray[2] = anotherrobot;
+                    return myarray;
+
+                }
+                else
+                {
+                    donor = (donorhp *0.7).ToString();
+                    robot = (robothp + 20).ToString();
+                    anotherrobot = (anotherrobothp + 20).ToString();
+                    myarray[0] = donor;
+                    myarray[1] = robot;
+                    myarray[2] = anotherrobot;
+                    return myarray;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Вы ввели не число в графе здоровья у робота");
+                return myarray;
+            }
+
+        }
+    }
+}
